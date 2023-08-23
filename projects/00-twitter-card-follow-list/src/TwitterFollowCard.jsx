09fbd856a7788f28df2a7b87fae8ad90ef985f6a
LuisSS20 <bobgenge@gmail.com>
2023-08-23 19:20:24 +0200

    Change some styles

--- projects/00-twitter-card-follow-list/src/TwitterFollowCard.jsx
+++ projects/00-twitter-card-follow-list/src/TwitterFollowCard.jsx
@@ -2,31 +2,12 @@ import { useState } from "react"
 
 export function TwitterFollowCard({name, userName, initialIsFollowing}){
 
-    const followText = "Follow";
-    const followingText = "Following";
-    const unfollowText = "Unfollow";
-
     const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
-    const [text, setText] = useState(initialIsFollowing ? followingText : followText)
+    const text = isFollowing ? 'Following' : 'Follow'
     const followButtonClass = isFollowing ? 'tw-card-follow-button button-is-following' : 'tw-card-follow-button'
 
-
     const followClick = () =>{
-        setIsFollowing(!isFollowing);
-        if(text == followText)
-            setText(unfollowText)
-        else
-            setText(followText)
-    }
-
-    const mouseEnterButton = () =>{
-        if(isFollowing)
-            setText(unfollowText)
-    }
-
-    const mouseLeaveButton = () =>{
-        if(isFollowing)
-            setText(followingText)
+        setIsFollowing(!isFollowing)
     }
 
     return (
@@ -39,8 +20,9 @@ export function TwitterFollowCard({name, userName, initialIsFollowing}){
                 </div>
             </header>
             <aside className="tw-card-aside">
-                <button className={followButtonClass} onClick={followClick} onMouseEnter={mouseEnterButton} onMouseLeave={mouseLeaveButton}>
+                <button className={followButtonClass} onClick={followClick}>
                     <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Unfollow</span>
                 </button>
             </aside>
         </article>
